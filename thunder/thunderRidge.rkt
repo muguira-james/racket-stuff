153d0cc6f4f9b8ec3794bad21b143a1563493bd8
muguira-james <muguira.james@gmail.com>
2022-01-02 14:20:15 -0500

    update source
    improve docs

--- thunder/thunderRidge.rkt
+++ thunder/thunderRidge.rkt
@@ -1,12 +1,18 @@
-
+;;
+;; Simulation
+;;
+;; simulate fire propagation along thunder ridge.
+;;
 #lang racket
 ; james
 (require 2htdp/universe 2htdp/image)
 (require lang/posn)
 
+;
+; constants for drawing
 (define WIDTH 420)
 (define HEIGHT 150)
-(define last-x 430)
+(define LAST-X 430)
 
 ;
 ; bring in the tree images from disk
@@ -17,60 +23,9 @@
 ;
 ; the node that holds our tree info
 (struct tree-struct (flag posn) #:transparent)
+
+; struct for handling the world
 (struct t-ridge (ridge) #:transparent)
-
-;
-; takes a field, full of place-for-tree structures, and draws them in a empty scene
-(define (show-ridge collection)
-  ; (displayln  collection)
-  (place-images (get-scene-images collection) (get-scene-positions collection) (empty-scene WIDTH HEIGHT)))
-
-
-; 
-; creates a single tree struct with a tree type, an x position and a y position
-(define (make-a-tree flag x y)
-  (tree-struct flag (make-posn x y)))
-
-(define (make-a-tree/p flag posn)
-  (make-a-tree flag (posn-x posn) (posn-y posn)))
-
-(define (contains-tree? t-struct)
-  (if (= 1 (tree-struct-flag t-struct))
-      #t
-      #f))
-
-(define (whip-up-tree? t-struct)
-  (let ((flag (random 2)))
-    (if (= 1 flag)
-        (make-a-tree/p flag (tree-struct-posn t-struct))
-        t-struct)))
-
-(define (grow-trees? f collection)
-  (for/list ((i collection))
-             (f i)))
-  
-
-;
-; randomly create a tree in a given position
-(define (create-tree/posn collection)
-  (cond [(empty? collection) empty]
-        [else
-         [cons (make-a-tree/p (random 2) (tree-struct-posn (first collection)))
-               (create-tree/posn (rest collection))]]))
-
-;
-; using the last x position, return a list of (possible) trees
-; since the x position counter starts at 30, to make 10 spots, n=430
-(define (make-ridge)
-  (for/list ([x (range 30 last-x 40)])
-                  (make-a-tree 0 x 90)))
-
-; (place-images (list pine pinefire) (list (make-posn 30 90) (make-posn 150 90)) (empty-scene WIDTH HEIGHT))
-;(define field (list
-;               (make-a-tree/p 0 (make-posn 30 90))
-;               (make-a-tree/p 1 (make-posn 70 90))
-;               (make-a-tree/p 1 (make-posn 110 90))
-;               (make-a-tree/p 2 (make-posn 150 90))))
 
 ;
 ; convert a flag to a image
@@ -97,28 +52,89 @@
              (get-scene-positions (rest collection)))]))
       
 
+;
+; takes a collection and renders on an empty scene
+(define (show-ridge collection)
+  ; (displayln  collection)
+  (place-images (get-scene-images collection) (get-scene-positions collection) (empty-scene WIDTH HEIGHT)))
+
+
+; 
+; creates a single tree struct with a tree type, an x position and a y position
+(define (make-a-tree flag x y)
+  (tree-struct flag (make-posn x y)))
+
+; create a tree using a position (posn)
+(define (make-a-tree/p flag posn)
+  (make-a-tree flag (posn-x posn) (posn-y posn)))
+
+;
+; does this square contain a tree?
+(define (contains-tree? t-struct)
+  (if (= 1 (tree-struct-flag t-struct))
+      #t
+      #f))
+
+;
+; decide if we  put a tree here
+(define (whip-up-tree? t-struct)
+  (let ((flag (random 2)))
+    (if (= 1 flag)
+        (make-a-tree/p flag (tree-struct-posn t-struct))
+        t-struct)))
+
+;
+; loop over the ridge call the input function, f
+(define (grow-trees? f collection)
+  (for/list ((i collection))
+             (f i)))
+  
+
+;
+; randomly create a tree in a given position - not used
+(define (create-tree/posn collection)
+  (cond [(empty? collection) empty]
+        [else
+         [cons (make-a-tree/p (random 2) (tree-struct-posn (first collection)))
+               (create-tree/posn (rest collection))]]))
+
+;
+; using the last x position, return a list of (possible) trees
+; since the x position counter starts at 30, to make 10 spots, n=430
+(define (make-ridge)
+  (for/list ([x (range 30 LAST-X 40)])
+                  (make-a-tree 0 x 90)))
+
+
 ;;;
 ;;;
 ;;  the rest here  is a ll world handlers
+;;;
+
+;
+; return a new ridge, populated with trees. This runs on the tick
 (define (tickr w)
     (define ridge (t-ridge-ridge w))
     (t-ridge (grow-trees? whip-up-tree? ridge)))
+    
 ;
-;
+; handle key presses
 (define (deal-with-guess w key)
   (cond 
         [(key=? key "q") (stop-with w)]
         [else w]))
+        
 ;
-;
+; draw the ridge scene
 (define (render w)
   ; (displayln w)
   (show-ridge (t-ridge-ridge w)))
+  
 ;
+; set up and run the simulation with "world", w
 (define  (start )
   (big-bang (t-ridge (make-ridge))
     (to-draw render)
     (on-key deal-with-guess)
     (on-tick tickr 2)))
 
-;; (define field (make-field 430))
