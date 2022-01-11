7c71e911f6c5ba51d61b1fab025052a59b85d1ac
muguira-james <muguira.james@gmail.com>
2022-01-11 17:41:38 -0500

    update the first version of crane flys

--- crane.rkt
+++ crane.rkt
@@ -1,6 +1,28 @@
 #lang racket
 (require 2htdp/universe 2htdp/image)
 
-(define width 400)
-(define height 300)
+(define WIDTH 600)
+(define HEIGHT 400)
 
+(define crane (scale/xy 0.25 0.25 (bitmap "flying-crane.png")))
+
+(struct bird-posn (x y))
+
+(define (render w)
+  (place-image crane (bird-posn-x w) (bird-posn-y w) (empty-scene WIDTH HEIGHT)))
+
+(define (handle-tick w)
+  (bird-posn (+ 10 (bird-posn-x w)) (bird-posn-y w)))
+
+(define (handle-key w key)
+  (cond
+    [(key=? key "q") (stop-with w)]
+    [else w]))
+
+(define (start)
+  (big-bang
+    (bird-posn 80 80)
+    (to-draw render)
+    (on-tick handle-tick)
+    (on-key handle-key)))
+  
